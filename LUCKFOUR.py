c7a7ca9cfebe7fdf4fdb128e2d2550ebe76d5104
AnveshAmbala <72644663+AnveshAmbala@users.noreply.github.com>
2021-07-21 20:10:24 +0530

    Add files via upload

---
+++ LUCKFOUR.py
@@ -0,0 +1,10 @@
+t = int(input())
+for i in range(t):
+    n = str(input())
+    count = 0
+    for i in range(len(n)):
+        
+        if n[i] == "4":
+            count += 1
+        
+    print(count)
